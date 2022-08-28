b994150010d5a9d5d2c5db4f2a21b1e3e6e3dad6
Caesar Augustus <wiccaesar@hotmail.com>
2022-08-27 22:26:05 -0300

    Initial files.

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnascime <cnascime@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 19:42:39 by cnascime          #+#    #+#             */
+/*   Updated: 2022/08/27 19:50:44 by cnascime         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 23
+# endif
+
+char	*get_next_line(int fd);
+#endif
